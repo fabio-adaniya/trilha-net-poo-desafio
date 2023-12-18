18295f1a616d8000ecc9cd0f86a7c25198040e1f
fabio-adaniya <f1hiroki@outlook.com>
2023-12-18 16:01:50 -0300

    Teste das classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123", "modelo 123", "1234", 16);
+Console.Write("Nokia ");
+nokia.Ligar();
+nokia.InstalarAplicativo("gov.br");
+
+Console.WriteLine("---------------------------------------------");
+
+Iphone iphone = new Iphone("456", "modelo 456", "4567", 32);
+Console.Write("Iphone ");
+nokia.Ligar();
+nokia.InstalarAplicativo("Waze");
